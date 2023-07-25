4bc34f02df28fa4aa9a9b778fdc7988e28c27840
ayoublatreb <ayoublatreb@gmail.com>
2023-07-25 12:48:59 +0200

    0

--- sort.h
+++ sort.h
@@ -1,7 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,10 +20,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
+
+/* print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
+
+/* sorting algorithms */
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
